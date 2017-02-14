092aea51bcb8fdb13c79e082a3b9f0e0479b41ea
Colin Bloom and Ryan McAlpin <ryan@>
2017-02-13 16:36:49 -0800

    cooking commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-var galLit = function(gal) {
-return gal * 3.785411784;
-}
-var gal = prompt("How many gallons?");
+jQuery(document).ready(function() {
+  var galLit = function(gal) {
+  return gal * 3.785411784;
+  }
+  var litGal = function(lit) {
+    return lit * 0.26417205;
+  }
+  var lit = prompt("How many liters");
 
-alert(galLit(gal));
+
+  alert(litGal(lit) + " gallons");
+});
